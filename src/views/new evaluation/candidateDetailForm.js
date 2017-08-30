6cc540fda239447d031dcfa385cfe3e914233244
Gavriil1994 <gavriil.petrov@softvision.ro>
2017-08-30 18:06:22 +0300

    Day 11

---
+++ src/views/new evaluation/candidateDetailForm.js
@@ -0,0 +1,10 @@
+const CandidateDetailsForm = function () {
+ return `
+ <section  class="CandidateForm ">
+ <input type="candidate" placeholder="Candidate">
+ <input type="interviewer" placeholder="Interviewer">
+ <input type="Date" placeholder="Date">
+</section>
+    `;
+    
+};
