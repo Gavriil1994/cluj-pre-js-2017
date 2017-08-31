8c103aa37a3f5461ddc3ce44412dc5f4a2f9833d
Gavriil1994 <gavriil.petrov@softvision.ro>
2017-08-31 17:56:02 +0300

    Work day 12

--- src/views/common/common.js
+++ src/views/common/common.js
@@ -11,12 +11,13 @@ const Footer = function() {
 }
 const NAV = function(options={}) {
     return `
-    <section>
-    <footer  id='footer'>
-        <div class="container">
-            <p class="footer-paragraph"> Copyright@Softvision 2017 </p>
-            </div>
-            </footer>
-   </section>
+    <section class="topnav" id="Topnav">
+    <img src="assets/logo.png" style="width:15%; height:30px;float:left">
+    <a href="#evaluations">Evaluations</a>
+    <a href="#new evaluation">New Evaluations</a>
+    <a style="float:right" href="#about">Logout</a>
+    <hr class="eval-header-bottom eval-header-bottom-skin">
+  </section>
+    `;
     
 }
