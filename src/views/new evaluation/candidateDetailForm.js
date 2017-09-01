34edf694ddc3c11052571e3b53809592594974ac
Sebastian Timofte <sebastian.timofte@softvision.ro>
2017-09-01 12:59:23 +0300

    fix conflicts and pagination

--- src/views/new evaluation/candidateDetailForm.js
+++ src/views/new evaluation/candidateDetailForm.js
@@ -1,10 +1,9 @@
 const CandidateDetailsForm = function () {
- return `
- <section  class="CandidateForm ">
- <input type="candidate" placeholder="Candidate">
- <input type="interviewer" placeholder="Interviewer">
- <input type="Date" placeholder="Date">
-</section>
+    return `
+        <section  class="CandidateForm ">
+            <input type="candidate" placeholder="Candidate">
+            <input type="interviewer" placeholder="Interviewer">
+            <input type="Date" placeholder="Date">
+        </section>
     `;
-    
 };
