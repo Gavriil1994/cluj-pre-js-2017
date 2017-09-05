db0b86307a6f244f1c29ae73c81a33045c8e75f9
Gavriil1994 <gavriil.petrov@softvision.ro>
2017-09-05 11:41:42 +0300

    Day14

---
+++ src/views/new evaluation/TechnicalAreaPicker.js
@@ -0,0 +1,14 @@
+const Component = function (options = {}) {
+    const arr = options.map(el =>
+    `<div class="content-box-fields">
+    <fieldset class="content-box-evaluations content-box-evaluations-skin">
+    
+${DropdownSelector(el)}
+</fieldset>
+</div>`);
+return arr.join('');
+};
+    
+const TechnicalAreaPicker = (options = {}) =>
+    
+`${Component(getTechnicalAreaData())}`;
